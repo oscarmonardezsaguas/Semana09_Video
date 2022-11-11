c85b713b724b44557e764f69093bc3c11439c3ec
Oscar Monardez Saguas <oscarmonardezsaguas@gmail.com>
2022-11-11 10:22:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/semana09_video/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.semana09_video;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView mivideo = findViewById(R.id.viVisor);
+
+        String videop="android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri =Uri.parse(videop);
+        mivideo.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mivideo.setMediaController(mediaController);
+        mediaController.setAnchorView(mivideo);
+
+
+    }
+}
